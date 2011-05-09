2ad9e9c354d1a7819ce3a8807fa42d8ad966ecfc
Dan Nachbar <dan@nachbar.com>
2011-05-09 16:11:45 -0400

    added more integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  
+
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
