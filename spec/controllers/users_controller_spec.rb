29714c5dbb43bdc2df107977ef615e539cef1c20
Dan Nachbar <dan@nachbar.com>
2011-08-02 16:48:32 -0400

    started on edit page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -109,9 +109,30 @@ describe UsersController do
         post :create, :user => @attr
         controller.should be_signed_in
       end
-      
     end
   end
   
+  describe "GET 'edit'" do
+    before (:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector('title', :content => "Edit user")
+    end
+    
+    it "should have a link to change the Gravatar" do
+      get :edit, :id =>@user
+      response.should have_selector('a', :href => 'http://gravatar.com/emails',
+                                          :content => 'change')
+    end
+    
+  end
 
 end
