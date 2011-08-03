ded9a0b4bc24472de805e78618ef2cc1b1f48042
Dan Nachbar <dan@nachbar.com>
2011-08-03 16:25:32 -0400

    working update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,5 +26,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = "Edit user"
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # it worked
+      redirect_to @user , :flash => {:success => "Profile updated!"}
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
 
 end
