52f899a619b68cdbf85d12209584cea564888701
Dan Nachbar <dan@nachbar.com>
2011-08-03 21:04:28 -0400

    finished pagination tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,7 @@ Factory.define :user do |user|
   user.password              "asdfasdf"
   user.password_confirmation "asdfasdf"
 end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
