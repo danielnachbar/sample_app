4b7a3773bd156ecd0e9de0938c320e8cdf32208b
Dan Nachbar <dan@nachbar.com>
2011-08-04 20:18:24 -0400

    added access controls to destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy ]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only =>  :destroy
   
   def index
     @users = User.paginate(:page => params[:page])
@@ -46,6 +47,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+#    redirect_to users_path, :flash => { :success => "User destroyed." }
+  end
+
   private
   
   def authenticate
@@ -57,6 +62,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
-
+ 
+  def admin_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user.admin?
+  end
 
 end
